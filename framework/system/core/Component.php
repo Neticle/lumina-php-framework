8ba39460130040a20316a3c318c5f6ef2cf81349
Pedro Bispo <pedro.bispo@neticle.pt>
2014-01-31 15:47:58 -0100

    Added Component class.

---
+++ framework/system/core/Component.php
@@ -0,0 +1,53 @@
+<?php
+
+// =============================================================================
+//
+// Copyright 2013 Neticle
+// http://lumina.neticle.com
+//
+// This file is part of "Lumina/PHP Framework", hereafter referred to as 
+// "Lumina".
+//
+// Lumina is free software: you can redistribute it and/or modify it under the 
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Lumina is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See theGNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// "Lumina". If not, see <http://www.gnu.org/licenses/>.
+//
+// =============================================================================
+
+namespace system\core;
+
+use \system\core\Extension;
+
+/**
+ * Defines the base behaviour and events for all Lumina components.
+ *
+ * @author Lumina Framework <lumina@incubator.neticle.com>
+ * @package system.core
+ * @since 0.2.0
+ */
+class Component extends Extension
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param Extension $parent
+	 *	The parent extension instance.
+	 *
+	 * @param array $configuration
+	 *	The component express configuration array.
+	 */
+	public final function __construct(Extension $parent, array $configuration = null)
+	{
+		parent::__construct($parent);
+		$this->construct($configuration);
+	}
+}
+
