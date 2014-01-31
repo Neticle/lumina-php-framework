092c12a78e3721d908108cc464e447df5c7c736c
Pedro Bispo <pedro.bispo@neticle.pt>
2014-01-31 09:05:47 -0100

    Added Exception and RuntimeException.

---
+++ framework/system/core/exception/Exception.php
@@ -0,0 +1,52 @@
+<?php
+
+// =============================================================================
+//
+// Copyright 2013 Neticle
+// http://lumina.neticle.com
+//
+// This file is part of "Lumina/PHP Framework", hereafter referred to as 
+// "Lumina".
+//
+// Lumina is free software: you can redistribute it and/or modify it under the 
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Lumina is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See theGNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// "Lumina". If not, see <http://www.gnu.org/licenses/>.
+//
+// =============================================================================
+
+namespace system\core;
+
+use \Exception as PHPException;
+
+/**
+ * A generic exception.
+ *
+ * @author Lumina Framework <lumina@incubator.neticle.com>
+ * @package system.core.exception
+ * @since 0.2.0
+ */
+abstract class Exception extends PHPException
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param string $message
+	 *	A human readable message describing the exception.
+	 *
+	 * @param PHPException $previous
+	 *	The previous exception instance, for chaining.
+	 */
+	public function __construct($message, $previous = null)
+	{
+		parent::__construct($message, 0, $previous);
+	}
+}
+
---
+++ framework/system/core/exception/RuntimeException.php
@@ -0,0 +1,54 @@
+<?php
+
+// =============================================================================
+//
+// Copyright 2013 Neticle
+// http://lumina.neticle.com
+//
+// This file is part of "Lumina/PHP Framework", hereafter referred to as 
+// "Lumina".
+//
+// Lumina is free software: you can redistribute it and/or modify it under the 
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Lumina is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See theGNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// "Lumina". If not, see <http://www.gnu.org/licenses/>.
+//
+// =============================================================================
+
+namespace system\core;
+
+use \system\core\exception\Exception;
+
+/**
+ * A runtime exception, which is usually thrown due to misconfigurations,
+ * incorrect argument types and other situations that are directly related
+ * to the application specific implementation.
+ *
+ * @author Lumina Framework <lumina@incubator.neticle.com>
+ * @package system.core.exception
+ * @since 0.2.0
+ */
+class RuntimeException extends Exception
+{
+	/**
+	 * Constructor.
+	 *
+	 * @param string $message
+	 *	A human readable message describing the exception.
+	 *
+	 * @param PHPException $previous
+	 *	The previous exception instance, for chaining.
+	 */
+	public function __construct($message, $previous = null)
+	{
+		parent::__construct($message, 0, $previous);
+	}
+}
+
