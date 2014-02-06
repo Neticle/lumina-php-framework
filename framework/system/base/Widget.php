382ddca82a9f09f029864c4832747f6be2346281
Pedro Bispo <pedro.bispo@neticle.pt>
2014-02-06 11:47:06 -0100

    Added Widget class.

--- framework/system/base/Widget.php
+++ framework/system/base/Widget.php
@@ -27,26 +27,71 @@ namespace system\base;
 use \system\core\Element;
 
 /**
- * Widgets are intended to dinamically generate output and is intended to speed 
- * up a project development time by removing the need to implement the same
+ * Widgets are intended to dinamically generate output and speed up
+ * a project development time by removing the need to implement the same
  * feature over and over again.
  *
  * @author Lumina Framework <lumina@incubator.neticle.com>
  * @package system.base
  * @since 0.2.0
  */
-abstract class Widget
+abstract class Widget extends Element
 {	
 	/**
+	 * The absolute path to the directory containing the widget files.
+	 *
+	 * @type string
+	 */
+	private $basePath;
+
+	/**
 	 * Constructor.
+	 *
+	 * @param string $basePath
+	 *	The absolute path to the directory containing the widget files, or
+	 *	NULL if applicable.
 	 *
 	 * @param array $configuration
 	 *	The express configuration array.
 	 */
-	public function __construct(array $configuration = null)
+	public function __construct($basePath, array $configuration = null)
 	{
 		parent::__construct($configuration);
+		$this->basePath = $basePath;
 	}
 	
+	/**
+	 * Returns the widget base path, when available.
+	 *
+	 * @return string
+	 *	The widget base path.
+	 */
+	public function getBasePath()
+	{
+		return $this->basePath;
+	}
+	
+	/**
+	 * Renders a context-less application view.
+	 *
+	 * @param string $view
+	 *	An alias resolving to the view being rendered, relative to the
+	 *	widget base path.
+	 *
+	 * @param array $variables
+	 *	The variables to be extracted into the script context.
+	 *
+	 * @param bool $capture
+	 *	When set to TRUE the rendered contents will be captured instead
+	 *	of sent to the currently active output buffer.
+	 *
+	 * @return string
+	 *	The captured contents, when applicable.
+	 */
+	public function render($view, array $variables = null, $capture = false)
+	{
+		$file = Lumina::getAliasPath($view, 'php', $this->basePath);
+		return View::getApplicationFileView($file, $variables)->run($capture);
+	}
 }
 
