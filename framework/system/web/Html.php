4f05086b5c6fa79b09b221f841e10738cedb87b2
Pedro Bispo <pedro.bispo@neticle.pt>
2014-02-06 11:56:40 -0100

    Added the Html class.

---
+++ framework/system/web/Html.php
@@ -0,0 +1,47 @@
+<?php
+
+// =============================================================================
+//
+// Copyright 2013 Neticle
+// http://lumina.neticle.com
+//
+// This file is part of "Lumina/PHP Framework", hereafter referred to as 
+// "Lumina".
+//
+// Lumina is free software: you can redistribute it and/or modify it under the 
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Lumina is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See theGNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// "Lumina". If not, see <http://www.gnu.org/licenses/>.
+//
+// =============================================================================
+
+namespace system\web;
+
+/**
+ * Provides static methods useful for the generation and validation
+ * of HTML content.
+ *
+ * @author Lumina Framework <lumina@incubator.neticle.com>
+ * @package system.web
+ * @since 0.2.0
+ */
+class Html
+{
+	/**
+	 * Encodes a value to a valid HTML string.
+	 *
+	 * @param string $value
+	 *	The value to encode.
+	 */
+	public static function encode($value)
+	{
+		return htmlentities($value, ENT_QUOTES | ENT_HTML401, 'UTF-8');
+	}
+}
