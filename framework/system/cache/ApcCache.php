86132e07a35f9dd89b477735a44f8abd0d240584
Pedro Bispo <pedro.bispo@neticle.pt>
2014-02-17 08:59:17 -0100

    Added APC cache component.

---
+++ framework/system/cache/ApcCache.php
@@ -0,0 +1,151 @@
+<?php
+
+// =============================================================================
+//
+// Copyright 2013 Neticle
+// http://lumina.neticle.com
+//
+// This file is part of "Lumina/PHP Framework", hereafter referred to as 
+// "Lumina".
+//
+// Lumina is free software: you can redistribute it and/or modify it under the 
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Lumina is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See theGNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// "Lumina". If not, see <http://www.gnu.org/licenses/>.
+//
+// =============================================================================
+
+namespace system\cache;
+
+use \system\cache\Cache;
+
+/**
+ * This component provides caching features through PHP's APC(u) extension.
+ *
+ * @author Lumina Framework <lumina@incubator.neticle.com>
+ * @package system.cache
+ * @since 0.2.0
+ */
+class ApcCache extends Cache
+{
+	/**
+	 * Opens or starts any resources required for cache management purposes.
+	 *
+	 * @throws RuntimeException
+	 *	Thrown when connection or initialization fails.
+	 */
+	public function open()
+	{
+		/* not implemented */ 
+	}
+	
+	/**
+	 * Closes or disposes any resources required for cache management purposes.
+	 *
+	 * @throws RuntimeException
+	 *	Thrown when the operation fails.
+	 */
+	public function close()
+	{
+		/* not implemented */ 
+	}
+	
+	/**
+	 * Returns a flag indicating the current status of this cache handler.
+	 *
+	 * @return bool
+	 *	Returns TRUE if the cache handle is closed, FALSE otherwise.
+	 */
+	public function isClosed()
+	{
+		return false;
+	}
+
+	/**
+	 * Returns a flag indicating wether or not this cache handler supports
+	 * persistent memory caching.
+	 *
+	 * @return bool
+	 *	Returns TRUE if this handler supports memory caching, FALSE otherwise.
+	 */
+	public function isMemoryCache()
+	{
+		return true;
+	}
+
+	/**
+	 * Writes a value to cache.
+	 *
+	 * @throws RuntimeException
+	 *	Thrown when the write operation fails.
+	 *
+	 * @param string $key
+	 *	The unique identifier for the value being cached.
+	 *
+	 * @param mixed $value
+	 *	The value to be cached, which may be converted to a string through the
+	 *	PHP serialization algorithm if this cache handler does not support
+	 *	persistent memory caching.
+	 *
+	 * @param int $expiry
+	 *	The amount of seconds until this key expires. When set to NULL the
+	 *	value will be cached for an undeterminated amount of time, until it's
+	 *	invalidated explicitly.
+	 */
+	public function write($key, $value, $expiry = null)
+	{
+		if (!isset($expiry))
+		{
+			$expiry = 0;
+		}
+		
+		if (!apc_store($key, $value, $expiry))
+		{
+			throw new RuntimeException('Unable to store "' . $key . '" in APC cache.');
+		}
+	}
+	
+	/**
+	 * Returns a previously cached value.
+	 *
+	 * @throws RuntimeException
+	 *	Thrown when the read operation fails.
+	 *
+	 * @param string $key
+	 *	The unique identifier of the value to return.
+	 *
+	 * @return mixed
+	 *	Returns the previously cached value or NULL if the value was not found
+	 *	or it's past its expiration date.
+	 */
+	public function read($key)
+	{
+		$result = apc_fetch($key, $success);
+		
+		if ($success)
+		{
+			return $result;
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Clears any cached value for the given key.
+	 *
+	 * @throws RuntimeException
+	 *	Thrown when the clear operation fails.
+	 */
+	public function clear($key)
+	{
+		apc_delete($key);
+	}
+}
+
---
+++ tests/framework/system/cache/cache_apc_001.php
@@ -0,0 +1,108 @@
+<?php
+
+// =============================================================================
+//
+// Copyright 2013 Neticle
+// http://lumina.neticle.com
+//
+// This file is part of "Lumina/PHP Framework", hereafter referred to as 
+// "Lumina".
+//
+// Lumina is free software: you can redistribute it and/or modify it under the 
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Lumina is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See theGNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// "Lumina". If not, see <http://www.gnu.org/licenses/>.
+//
+// =============================================================================
+
+/*
+
+// MySQL:
+
+CREATE DATABASE sakila 
+	DEFAULT COLLATE utf8_general_ci 
+	DEFAULT CHARSET utf8;
+
+CREATE TABLE lumina_test_table_002 (
+
+id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+
+column1 VARCHAR(255) NOT NULL,
+column2 TINYINT NOT NULL,
+column3 CHAR(1) NOT NULL,
+column4 BLOB NULL,
+column5 TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+
+CONSTRAINT lumina_test_table_002_pk_id
+PRIMARY KEY (id)
+);
+
+*/
+
+use \system\core\Lumina;
+use \system\sql\Expression;
+use \system\sql\data\Record;
+
+define('L_APPLICATION_ROOT', dirname(__FILE__));
+require '../../../../framework/bootstrap.php';
+require '../../../lumina.php';
+
+lumina_test_start('Record Cache');
+
+$app = Lumina::loadWebApplication(array(
+	
+	'components' => array(
+		'database' => array(
+			'dsn' => 'host=127.0.0.1;dbname=sakila;charset=utf8',
+			'user' => 'root',
+			'password' => 'password'
+		),
+		
+		'cache' => array(
+			'class' => 'system\\cache\\ApcCache'
+		)
+	)
+	
+));
+
+class TestTable2 extends Record
+{
+	public static function model($context = 'search')
+	{
+		return self::getBaseModel(__CLASS__, $context);
+	}
+
+	protected function getTableName()
+	{
+		return 'lumina_test_table_002';
+	}
+}
+
+$cache = $app->getComponent('cache');
+$count = $cache->read('lumina.test.cache.record001.count');
+
+if (isset($count))
+{
+	lumina_test_report('Cached Result Available', true, array(
+		'count' => $count
+	));
+}
+else
+{
+	$count = TestTable2::model()->count();
+
+	lumina_test_report('Cached Result Not Available', true, array(
+		'count' => $count
+	));
+	
+	$cache->write('lumina.test.cache.record001.count', $count, 5);
+}
+
+lumina_test_end();
