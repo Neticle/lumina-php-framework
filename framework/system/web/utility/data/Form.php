0c04a9a5c4878e985a9d541fe1cda95824cdd89a
Pedro Bispo <pedro.bispo@neticle.pt>
2014-04-06 12:47:03 +0000

    Added Form utility class.

---
+++ framework/system/web/utility/data/Form.php
@@ -0,0 +1,352 @@
+<?php
+
+// =============================================================================
+//
+// Copyright 2013 Neticle
+// http://lumina.neticle.com
+//
+// This file is part of "Lumina/PHP Framework", hereafter referred to as 
+// "Lumina".
+//
+// Lumina is free software: you can redistribute it and/or modify it under the 
+// terms of the GNU General Public License as published by the Free Software 
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Lumina is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See theGNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// "Lumina". If not, see <http://www.gnu.org/licenses/>.
+//
+// =============================================================================
+
+namespace system\web\utility\data;
+
+use \system\core\Element;
+use \system\data\Model;
+use \system\web\html\HtmlElement;
+
+/**
+ * An utility class that allows the developer to quickly create consitent views
+ * which purpose is to collect input data to be bound to a model or record
+ * instance, as well as present any errors detected during the validation
+ * stage.
+ *
+ * @author Lumina Framework <lumina@incubator.neticle.com>
+ * @package system.web
+ * @since 0.2.0
+ */
+class Form extends Element
+{	
+	/**
+	 * Constructor.
+	 *
+	 * @param array $configuration
+	 *	The express configuration array.
+	 */
+	public function __construct(array $configuration = null)
+	{
+		parent::__construct($configuration);
+	}
+	
+	/**
+	 * Builds a simple text field element.
+	 *
+	 * @param string $name
+	 *	The name of the input field to build.
+	 *
+	 * @param string $value
+	 *	The initial value for this input field.
+	 *
+	 * @param array $configuration
+	 *	An array of additional express configuration settings to be
+	 *	applied to the input field element, after it's construction.
+	 *
+	 * @return HtmlElement
+	 *	The created input field element.
+	 */
+	protected function buildTextField($name, $value, array $configuration = null)
+	{
+		$input = new HtmlElement('input');
+		$input->setClass(array('lh-form-input', 'lh-form-input-textfield'));
+		$input->setAttributes(array(
+			'name' => $name,
+			'value' => $value
+		));
+		
+		if (isset($configuration))
+		{
+			$input->configure($configuration);
+		}
+		
+		return $input;
+	}
+	
+	/**
+	 * Builds a simple textarea field element.
+	 *
+	 * @param string $name
+	 *	The name of the input field to build.
+	 *
+	 * @param string $value
+	 *	The initial value for this input field.
+	 *
+	 * @param array $configuration
+	 *	An array of additional express configuration settings to be
+	 *	applied to the input field element, after it's construction.
+	 *
+	 * @return HtmlElement
+	 *	The created input field element.
+	 */
+	protected function buildTextArea($name, $value, array $configuration = null)
+	{
+		$textarea = new HtmlElement('input');
+		$textarea->setClass(array('lh-form-input', 'lh-form-input-textarea'));
+		$textarea->setAttribute('name', $name);
+		
+		if (isset($value))
+		{
+			$textarea->setTextContent($value);
+		}
+		
+		if (isset($configuration))
+		{
+			$textarea->configure($configuration);
+		}
+		
+		return $textarea;
+	}
+	
+	/**
+	 * Builds a small report listing the errors for a specific
+	 * input field.
+	 *
+	 * @param string[] $messages
+	 *	The messages to be contained in the report.
+	 */
+	protected function buildInputErrors(array $messages)
+	{
+		$content = array();
+		
+		foreach ($messages as $message)
+		{
+			$li = new HtmlElement('li');
+			$li->setClass(array('lh-form-input-error'));
+			$li->setTextContent($message);
+			$content[] = $li;
+		}
+		
+		$ul = new HtmlElement('ul');
+		$ul->setClass(array('lh-form-input-errors'));
+		$ul->setContent($content);
+		return $ul;
+	}
+	
+	/**
+	 * Builds a report listing the validation errors detected across all
+	 * input fields defined within the form.
+	 *
+	 * @param string[] $messages
+	 *	The messages to be contained in the report.
+	 */
+	protected function buildFormErrors(array $messages)
+	{
+		$content = array();
+		
+		foreach ($messages as $message)
+		{
+			$li = new HtmlElement('li');
+			$li->setClass(array('lh-form-error'));
+			$li->setTextContent($message);
+			$content[] = $li;
+		}
+		
+		$ul = new HtmlElement('ul');
+		$ul->setClass(array('lh-form-errors'));
+		$ul->setContent($content);
+		return $ul;
+	}
+	
+	/**
+	 * Builds and deploys a text input field.
+	 *
+	 * @param string $name
+	 *	The name of the field to deploy.
+	 *
+	 * @param string $value
+	 *	The initial value for this input field.
+	 *
+	 * @param array $configuration
+	 *	An additional express configuration array to configure the input
+	 *	html element with.
+	 */
+	public function textField($name, $value, array $configuration = null)
+	{
+		return $this->buildTextField($name, $value, $configuration)->render();
+	}
+	
+	/**
+	 * Builds and deploys a text input field.
+	 *
+	 * @param Model $model
+	 *	The model to build the input for.
+	 *
+	 * @param string $attribute
+	 *	The model attribute to build the input for.
+	 *
+	 * @param array $configuration
+	 *	An additional express configuration array to configure the input
+	 *	html element with.
+	 */
+	public function activeTextField(Model $model, $attribute, array $configuration = null)
+	{
+		$input = $this->buildTextField(
+			$model->getAttributeName($attribute),
+			$model->getAttribute($attribute),
+			$configuration
+		);
+	
+		if ($model->hasAttributeErrors($attribute))
+		{
+			$input->setClass('lh-form-input-error');
+		}
+		
+		return $input->render();
+	}
+	
+	/**
+	 * Builds and deploys a textarea input field.
+	 *
+	 * @param string $name
+	 *	The name of the field to deploy.
+	 *
+	 * @param string $value
+	 *	The initial value for this input field.
+	 *
+	 * @param array $configuration
+	 *	An additional express configuration array to configure the input
+	 *	html element with.
+	 */
+	public function textArea($name, $value, array $configuration = null)
+	{
+		return $this->buildTextArea($name, $value, $configuration)->render();
+	}
+	
+	/**
+	 * Builds and deploys a textarea input field.
+	 *
+	 * @param Model $model
+	 *	The model to build the input for.
+	 *
+	 * @param string $attribute
+	 *	The model attribute to build the input for.
+	 *
+	 * @param array $configuration
+	 *	An additional express configuration array to configure the input
+	 *	html element with.
+	 */
+	public function activeTextArea(Model $model, $attribute, array $configuration = null)
+	{
+		$input = $this->buildTextArea(
+			$model->getAttributeName($model, $attribute),
+			$model->getAttribute($attribute),
+			$configuration
+		);
+		
+		if ($model->hasAttributeErrors($attribute))
+		{
+			$input->setClass('lh-form-input-error');
+		}
+		
+		return $input;
+	}
+	
+	/**
+	 * Builds and deploys a validation error report for a specific
+	 * input.
+	 *
+	 * @param string[] $messages
+	 *	The messages to contain in the generated report. If NULL or an
+	 *	empty array is given the report will not be generated.
+	 */
+	public function inputErrors(array $messages = null)
+	{
+		if (!empty($messages))
+		{
+			$this->buildInputErrors($messages)->render();
+		}
+	}
+	
+	/**
+	 * Builds and deploys a validation error report for a specific
+	 * input.
+	 *
+	 * @param Model $model
+	 *	The model to build the error report for.
+	 *
+	 * @param string $attribute
+	 *	The model attribute to build the error report for. If no errors are
+	 *	reported for this attribute the report will not be generated.
+	 */
+	public function activeInputErrors(Model $model, $attribute)
+	{
+		$this->inputErrors($model->getAttributeErrorMessages($attribute));
+	}
+	
+	/**
+	 * Builds and deploys a report listing the validation errors detected across
+	 * all input fields defined within the form.
+	 *
+	 * @param string[] $messages
+	 *	The messages to be contained in the report.
+	 */
+	public function formErrors(array $messages = null)
+	{
+		if (!empty($messages))
+		{
+			$this->buildFormErrors($messages)->render();
+		}
+	}
+	
+	/**
+	 * Builds and deploys a report listing the validation errors detected across
+	 * all input fields defined within the form.
+	 *
+	 * @param Model|string[]|string ...
+	 *	A model to extract the messages from, an array of messages or a single
+	 *	message to present in the report.
+	 */
+	public function activeFormErrors(/* ... */)
+	{
+		$messages = array();
+	
+		foreach (func_get_args() as $i => $value)
+		{
+			if ($value instanceof Model)
+			{
+				$messages = array_merge($messages, $value->getAttributeErrorMessages());
+			}
+			
+			else if (is_array($value))
+			{
+				$messages = array_merge($messages, $value);
+			}
+			
+			else if (is_string($value))
+			{
+				$messages[] = $value;
+			}
+			
+			else
+			{
+				throw new RuntimeException('Invalid argument type ("' . gettype($value) . '") at index ' . $i . '.');
+			}
+		}
+		
+		$this->formErrors(array_unique($messages));
+	}
+	
+}
+
