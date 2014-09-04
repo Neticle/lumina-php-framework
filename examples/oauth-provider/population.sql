c08ac1de06d128e5df75693823e9f2a0b00c3462
Igor Azevedo <igor.azevedo@neticle.pt>
2014-09-04 12:06:20 +0000

    Fixed wrong database name on population script

--- examples/oauth-provider/population.sql
+++ examples/oauth-provider/population.sql
@@ -1,4 +1,4 @@
-USE `ntcl_control_dev2`;
+USE `lumina_examples_oauth_provider`;
 
 INSERT INTO `user` (`username`, `password`, `active`) VALUES 
 	('testuser', '$2y$10$aJJVT.SzsKPJsACFFahDZeR9mf/MkpKlPkLtE2px3ten3n1nQgq.W', 1);
